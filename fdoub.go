b0675d3d79643702db44c5cbc8f0100797d47e99
esote <esote.net@gmail.com>
2020-03-03 14:44:53 -0600

    Use Go, math/big for arbitrarily-large values

---
+++ fdoub.go
@@ -0,0 +1,44 @@
+package fdoub
+
+import "math/big"
+
+var (
+	zero = new(big.Int)
+	one  = big.NewInt(1)
+	two  = big.NewInt(2)
+)
+
+// A fast fibonacci calculator through fast doubling.
+// https://en.wikipedia.org/wiki/Fibonacci_number#Matrix_form
+func Fib(n *big.Int) *big.Int {
+	n, _ = fib(n)
+	return n
+}
+
+func fib(n *big.Int) (*big.Int, *big.Int) {
+	if n.Cmp(zero) == 0 {
+		return zero, one
+	}
+
+	m := new(big.Int)
+	n, m = n.DivMod(n, two, m)
+
+	even := m.Cmp(zero) == 0
+
+	a, b := fib(n)
+
+	c, d := m, new(big.Int)
+	c = c.Mul(a, a)
+	d = d.Mul(b, b)
+	d = d.Add(d, c)
+	c = c.Mul(b, two)
+	c = c.Sub(c, a)
+	c = c.Mul(c, a)
+
+	if even {
+		return c, d
+	}
+
+	c = c.Add(c, d)
+	return d, c
+}
--- fdoub.h
+++
@@ -1,8 +0,0 @@
-#ifndef FDOUB_H
-#define FDOUB_H
-
-#include <stdint.h>
-
-uint_fast64_t fib(uint_fast64_t const n);
-
-#endif
